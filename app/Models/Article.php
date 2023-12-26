b8f352e2da22dd85b7a2a7d70a897c32e095663d
Hamidreza Safayee <hamidreza.safayee@gmail.com>
2023-12-26 15:00:59 +0330

    create filament article resource

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,14 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
-    protected $fillable = ['title', 'body', 'image', 'user_id'];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = ['title', 'body', 'image'];
 }
+
--- database/migrations/2023_12_26_105244_create_articles_table.php
+++ database/migrations/2023_12_26_105244_create_articles_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('title');
             $table->text('body');
             $table->string('image');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
