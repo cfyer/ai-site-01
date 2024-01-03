db8edb547d7713a223d07ebc2b0b0392402b14b1
Hamidreza Safayee <hamidreza.safayee@gmail.com>
2024-01-03 20:19:26 +0330

    fix stats chart bug

--- app/Services/Statistics/ChartData.php
+++ app/Services/Statistics/ChartData.php
@@ -9,12 +9,14 @@ class ChartData
 {
     public static function calculate(Model $model, int $days = 7): array
     {
+        $startDate = Carbon::now()->subDays($days);
+        $endDate = Carbon::now();
+
         $counts = [];
 
-        for ($i = 0; $i < $days; $i++) {
-            $date = Carbon::now()->copy()->subDays($i)->toDateString();
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
 
-            $count = $model::query()->whereDate('created_at', $date)->count();
+            $count = $model::query()->whereDate('created_at', $date->toDateString())->count();
 
             $counts[] = $count;
         }
